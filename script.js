0f3afa77df89e405a88d4fb2c6ac067271940fbf
emanuelle1301 <emanuelle.ferreira.costa@escola.pr.gov.br>
2025-09-10 13:34:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventL1stener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttibute('data-proximo');
+
+        atual.classL1st.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
